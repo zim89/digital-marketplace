00ad1d1812db8aa5917dc467097fcc0eaa47525f
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-17 17:29:36 +0200

    add home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 import type { ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,9 +16,18 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
+  'scsc'.toLowerCase();
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className='h-full'>
+      <body
+        className={cn(
+          'relative h-full font-sans antialiased',
+          inter.className
+        )}>
+        <main className='relative flex min-h-screen flex-col'>
+          <div className='flex-1 flex-grow'>{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
